ac30316d5f60c951d3f02b60d3b1cf3dafcadbe1
John Lima <thejohnlima@icloud.com>
2021-07-02 00:19:53 -0400

    Remove dead code

--- Sources/LMLoading/LMLoading.swift
+++ Sources/LMLoading/LMLoading.swift
@@ -53,36 +53,6 @@ public class LMLoading {
   }
 
   // MARK: - Public Methods
-  @available(iOS 15.0, *)
-  public static func present(loading: LMLoadingType,
-                             target: UIViewController,
-                             view: UIView? = nil,
-                             styleFull: Bool = true,
-                             alphaBackground: CGFloat = 0.75,
-                             tintColor: UIColor? = nil) async -> Bool {
-    await withCheckedContinuation { continuation in
-      show(
-        loading: loading,
-        target: target,
-        view: view,
-        styleFull: styleFull,
-        alphaBackground: alphaBackground,
-        tintColor: tintColor
-      ) {
-        continuation.resume(returning: true)
-      }
-    }
-  }
-
-  @available(iOS 15.0, *)
-  public static func dismiss(_ animation: LMLoadingType? = nil) async -> Bool {
-    await withCheckedContinuation { continuation in
-      hide(animation) {
-        continuation.resume(returning: true)
-      }
-    }
-  }
-
   public static func show(loading: LMLoadingType,
                           target: UIViewController,
                           view: UIView? = nil,
